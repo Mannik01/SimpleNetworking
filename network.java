08c65a0860c1079dcc308545ccafb62cd8d83277
MandipSilwal <mannik456@gmail.com>
2015-11-23 13:58:20 -0600

    First commit of network programming

---
+++ network.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.net.*;
+
+public class network
+{
+	public static void main(String args[])
+	{
+		try
+		{
+			URL newUrl = new URL("http://www.oracle.com/");	
+			BufferedReader in = new BufferedReader(new InputStreamReader(newUrl.openStream()));
+
+			String line;
+			while((line = in.readLine()) != null)
+			{
+				System.out.println(line);
+			}
+
+			in.close();
+		}
+		catch(MalformedURLException e)
+		{
+			System.out.println(e);
+		}
+		catch(IOException e)
+		{
+			System.out.println(e);
+		}
+		
+	}
+	
+
+}
